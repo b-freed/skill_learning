fab380d5f4fd8e46142faee264a012c7ee9d211c
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-07-26 16:23:50 +0530

    Update train_skills.py

--- train_skills.py
+++ train_skills.py
@@ -6,19 +6,21 @@ from torch.utils.data import TensorDataset
 from torch.utils.data.dataloader import DataLoader
 import torch.distributions.normal as Normal
 from skill_model import SkillModel
+from episode_data_buffer import EpisodeDataBuffer
 
 import d4rl
 
 
 
 def train(model,model_optimizer):
-
+	
+	loader = EpisodeDataBuffer.get_dataloader()
 	losses = []
 	s_T_losses = []
 	a_losses = []
 	kl_losses = []
 
-	for batch_id, (data, target) in enumerate(train_loader):
+	for batch_id, (data, target) in enumerate(loader):
 		data, target = data.cuda(), target.cuda()
 		states = data[:,:,:model.state_dim]  # first state_dim elements are the state
 		actions = data[:,:,model.state_dim:]	 # rest are actions
