06816cf764cc50b001b396135b046fea9d26dee4
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-07-27 00:05:18 +0530

    Update train_skills.py

--- train_skills.py
+++ train_skills.py
@@ -6,7 +6,6 @@ from torch.utils.data import TensorDataset
 from torch.utils.data.dataloader import DataLoader
 import torch.distributions.normal as Normal
 from skill_model import SkillModel
-from episode_data_buffer import EpisodeDataBuffer
 
 import d4rl
 
@@ -14,13 +13,12 @@ import d4rl
 
 def train(model,model_optimizer):
 	
-	loader = EpisodeDataBuffer.get_dataloader()
 	losses = []
 	s_T_losses = []
 	a_losses = []
 	kl_losses = []
 
-	for batch_id, (data, target) in enumerate(loader):
+	for batch_id, (data, target) in enumerate(train_loader):
 		data, target = data.cuda(), target.cuda()
 		states = data[:,:,:model.state_dim]  # first state_dim elements are the state
 		actions = data[:,:,model.state_dim:]	 # rest are actions
@@ -43,7 +41,7 @@ def train(model,model_optimizer):
 
 # First, instantiate a skill model
 model = SkillModel()
-model_optimizer = torch.optim.Adam(model.parameters(), lr=0.0001) # default lr-0.0001
+model_optimizer = torch.optim.Adam(model.parameters(), lr=0.002) # default lr-0.0001
 
 # instantiating the environmnet, getting the dataset.
 # the data is in a big dictionary, containing long sequences of obs, rew, actions, goals
