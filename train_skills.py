7b063b5b9d3e28bdc8fb3a84d73fa14c043b50c3
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-07-28 16:39:19 +0530

    Update train_skills.py

--- train_skills.py
+++ train_skills.py
@@ -61,7 +61,7 @@ paths = collections.defaultdict(list)
 initial_goal = data['infos/goal'][0]
 
 # add chunks of data to a pytorch dataloader
-inputs = torch.cat([paths,actions],axis=-1) # array that is dataset_size x T x state_dim+action_dim 
+inputs = np.concatenate([paths,actions],axis=-1) # array that is dataset_size x T x state_dim+action_dim 
 # targets = data['infos/goal'] can be anyhing, maybe make this the goals but we probably won't use it
 train_data = TensorDataset(torch.tensor(inputs, dtype=torch.float32)) # ,torch.tensor(targets,dtype=torch.float32))
 
