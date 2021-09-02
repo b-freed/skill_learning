ffacb974c39834483b10c1adb2191f7a5e97adb9
b-freed <bfreed@andrew.cmu.edu>
2021-09-02 10:37:01 -0400

    fix issue where rnn was outputing a tuple

--- skill_model.py
+++ skill_model.py
@@ -149,11 +149,13 @@ class Encoder(nn.Module):
 
         # concatenate states and actions
         s_a = torch.cat([states,actions],dim=-1)
+        print('s_a: ', s_a)
         # embedd states and actions
         embed = self.emb_layer(s_a)
         # through rnn
-        feats = self.rnn(embed)
+        feats,hn = self.rnn(embed)
         # get z_mean and z_sig by passing rnn output through mean_layer and sig_layer
+        print('feats: ', feats)
         z_mean = self.mean_layer(feats)
         z_sig = self.sig_layer(feats)
         
