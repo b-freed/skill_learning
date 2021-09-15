503584f3bdf040c42ed2de49e1c271d532b754c4
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-09-15 16:50:18 +0530

    Update skill_model.py

--- skill_model.py
+++ skill_model.py
@@ -149,14 +149,11 @@ class Encoder(nn.Module):
 
         # concatenate states and actions
         s_a = torch.cat([states,actions],dim=-1)
-        print(s_a.size())
         # embedd states and actions
         embed = self.emb_layer(s_a)
-        print(embed.size())
         # through rnn
         feats,hn = self.rnn(embed)
         # get z_mean and z_sig by passing rnn output through mean_layer and sig_layer
-        print(feats.size())
         z_mean = self.mean_layer(feats)
         z_sig = self.sig_layer(feats)
         
