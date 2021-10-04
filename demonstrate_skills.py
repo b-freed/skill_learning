65059f413ad454407eb68712e759cefd82ed84b6
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-10-04 16:30:01 +0530

    Update demonstrate_skills.py

--- demonstrate_skills.py
+++ demonstrate_skills.py
@@ -17,6 +17,7 @@ from skill_model import SkillModel
 import gym
 from pointmass_env import PointmassEnv
 
+torch.set_default_dtype(torch.float64)
 device = torch.device('cuda:0')
 
 filename = 'log.pth'
