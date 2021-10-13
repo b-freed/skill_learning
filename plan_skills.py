606126a6eae393194635313552f1a5f7af44b131
Ben Freed <42872052+b-freed@users.noreply.github.com>
2021-10-13 10:26:25 -0400

    add more comments

--- plan_skills.py
+++ plan_skills.py
@@ -1,10 +1,14 @@
 '''File where we will sample a set of waypionts, and plan a sequence of skills to have our pointmass travel through those waypoits'''
 
 
-
+# initialize skill sequence
+# initialize optimizer for skill sequence
+# determine waypoints
 
 
 for e in epochs:
   # Optimize plan: compute expected cost according to the current sequence of skills, take GD step on cost to optimize skills
   
   # Test plan: deploy learned skills in actual environment.  Now we're going be selecting base-level actions conditioned on the current skill and state, and executign that action in the real environment
+
+  # compute test and train plan cost, plot so we can see what they;re doing
