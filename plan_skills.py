0923e8e9ec3fde0ba7f759a388887d486e309783
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-10-20 17:49:46 +0530

    added optimize plan

--- plan_skills.py
+++ plan_skills.py
@@ -23,9 +23,15 @@ seq_optimizer = torch.optim.Adam(skill_seq, lr=0.002)
 # determine waypoints
 goal_seq = 2*torch.rand((1,H,s_dim)) - 1
 
+total_cost = 0
+
 for e in range(epochs):
   # Optimize plan: compute expected cost according to the current sequence of skills, take GD step on cost to optimize skills
-  
+  exp_cost = SkillModel.get_expected_cost(s0, skill_seq, goal_seq)
+  seq_optimizer.zero_grad()
+  seq_optimizer.step()
+
+  total_cost += exp_cost
   # Test plan: deploy learned skills in actual environment.  Now we're going be selecting base-level actions conditioned on the current skill and state, and executign that action in the real environment
 
   # compute test and train plan cost, plot so we can see what they;re doing
