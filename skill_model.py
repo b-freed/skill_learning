bec0c5c394abb53c57bc12e37aff7be4b62ef77a
b-freed <bfreed@andrew.cmu.edu>
2021-11-17 12:14:43 -0500

    remove unused AbstractDynamics from SkillModel.  We should use the one from the decoder instead.

--- skill_model.py
+++ skill_model.py
@@ -298,6 +298,7 @@ class SkillModel(nn.Module):
         s_T = s_T.unsqueeze(1) # adds extra dimension along time axis
         s_T_loss = -torch.mean(torch.sum(s_T_dist.log_prob(s_T),dim=-1))/T # divide by T because all other losses we take mean over T dimension, effectively dividing by T
         a_loss   = -torch.mean(torch.sum(a_dist.log_prob(actions),dim=-1))
+        print('a_dist.log_prob(actions): ',a_dist.log_prob(actions))
         # loss term correpsonding ot kl loss between posterior and prior
         # kl_loss = torch.mean(torch.sum(F.kl_div(z_post_dist, z_prior_dist),dim=-1))
         kl_loss = torch.mean(torch.sum(KL.kl_divergence(z_post_dist, z_prior_dist), dim=-1))
