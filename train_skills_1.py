3c3a41c7127b8e2e67a53acea357d75bbcdb83b7
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-11-20 02:41:12 +0530

    Added validation fn to code

--- train_skills_1.py
+++ train_skills_1.py
@@ -36,8 +36,30 @@ def train(model,model_optimizer):
 		a_losses.append(a_loss.item())
 		kl_losses.append(kl_loss.item())
 
-	return np.mean(losses), np.mean(s_T_losses), np.mean(a_losses), np.mean(kl_losses) 
+	return np.mean(losses), np.mean(s_T_losses), np.mean(a_losses), np.mean(kl_losses)
 
+def validate(model):
+
+    val_losses = []
+    val_s_T_losses = []
+    val_a_losses = []
+    val_kl_losses = []
+
+    for batch_id, (data, target) in enumerate(test_loader):
+        data, target = data.cuda(), target.cuda()
+        states = data[:,:,:model.state_dim]
+        actions = data[:,:,model.state_dim:]
+
+        with torch.no_grad():
+            val_loss_tot, val_s_T_loss, val_a_loss, val_kl_loss = model.get_losses(states, actions)
+        
+        # log losses
+        val_losses.append(val_loss_tot.item())
+        val_s_T_losses.append(val_s_T_loss.item())
+        val_a_losses.append(val_a_loss.item())
+        val_kl_losses.append(val_kl_loss.item())
+
+    return np.mean(val_losses), np.mean(val_s_T_losses), np.mean(val_a_losses), np.mean(val_kl_losses) 
 
 batch_size = 100
 
@@ -78,8 +100,10 @@ def get_data():
 
 if __name__ == '__main__':
 	experiment = Experiment(api_key = 'yQQo8E8TOCWYiVSruS7nxHaB5', project_name = 'skill-learning', workspace="anirudh-27")
-	experiment.add_tag('kl loss divided by T')
+	experiment.add_tag('Validation')
 
+	'''FOR TRAINING'''
+	
 	states, actions, goals = get_data()
 	state_dim = states.shape[2]
 	a_dim = actions.shape[2]
@@ -95,7 +119,6 @@ if __name__ == '__main__':
 	experiment.log_parameters({'lr':lr,
 							   'h_dim':h_dim})
 
-	# add chunks of data to a pytorch dataloader
 	inputs = np.concatenate([states, actions],axis=-1) # array that is dataset_size x T x state_dim+action_dim 
 	targets = goals
 	train_data = TensorDataset(torch.tensor(inputs, dtype=torch.float32), torch.tensor(targets,dtype=torch.float32))
@@ -103,26 +126,65 @@ if __name__ == '__main__':
 	train_loader = DataLoader(
 		train_data,
 		batch_size=batch_size,
-		num_workers=0)  # not really sure about num_workers...
+		num_workers=0)  
 
-	
+	'''FOR VALIDATION'''
+
+	states_val, actions_val, goals_val = get_data()
+	inputs_val = np.concatenate([states_val, actions_val],axis=-1)
+	targets_val = goals_val
+	test_data = TensorDataset(torch.tensor(inputs_val, dtype=torch.float32), torch.tensor(targets_val,dtype=torch.float32))
+
+	test_loader = DataLoader(
+		test_data,
+		batch_size=batch_size,
+		num_workers=0) 
+
+	min_test = 10**10
 	for i in range(n_epochs):
+
+		'''TRAINING'''
+		print('TRAINING-------------------------------------------------')
+		
 		loss, s_T_loss, a_loss, kl_loss = train(model,model_optimizer)
+
 		print('loss: ', loss)
 		print('s_T_loss: ', s_T_loss)
 		print('a_loss: ', a_loss)
 		print('kl_loss: ', kl_loss)
-		print(i)
+		print('EPOCH:', i)
+
 		experiment.log_metric("loss", loss, step=i)
 		experiment.log_metric("s_T_loss", s_T_loss, step=i)
 		experiment.log_metric("a_loss", a_loss, step=i)
 		experiment.log_metric("kl_loss", kl_loss, step=i)
 
+		'''VALIDATION'''
+		print('VALIDATION------------------------------------------------')
+		
+		val_loss, val_s_T_loss, val_a_loss, val_kl_loss = validate(model)
+
+		print('val_loss: ', val_loss)
+		print('val_s_T_loss: ', val_s_T_loss)
+		print('val_a_loss: ', val_a_loss)
+		print('val_kl_loss: ', val_kl_loss)
+		print('EPOCH:', i)
+
+		experiment.log_metric("val_loss", val_loss, step=i)
+		experiment.log_metric("val_s_T_loss", val_s_T_loss, step=i)
+		experiment.log_metric("val_a_loss", val_a_loss, step=i)
+		experiment.log_metric("val_kl_loss", val_kl_loss, step=i)
+
 
 		if i % 10 == 0:
 			filename = 'log.pth'
-			checkpoint_path = 'checkpoints/'+filename
-			torch.save({
-							'model_state_dict': model.state_dict(),
-							'model_optimizer_state_dict': model_optimizer.state_dict(),
-							}, checkpoint_path)
+			checkpoint_path = 'checkpoints/'
+			torch.save({'model_state_dict': model.state_dict(),
+						'model_optimizer_state_dict': model_optimizer.state_dict(),
+					}, checkpoint_path+filename)
+		
+		if val_loss < min_test:
+			min_test = val_loss
+			torch.save({'model_state_dict': model.state_dict(),
+						'model_optimizer_state_dict': model_optimizer.state_dict(),
+						}, checkpoint_path+'log_best.pth')
