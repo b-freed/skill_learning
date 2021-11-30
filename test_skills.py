ffaccec22d4eb794977f18cb6f199d6b0acbf898
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-12-01 04:28:22 +0530

    Update test_skills.py

--- test_skills.py
+++ test_skills.py
@@ -117,8 +117,8 @@ for i in range(episodes):
 
 	z = skill_model_sdp.reparameterize(z_mean,z_sig)
 	sT_mean,sT_sig = skill_model_sdp.decoder.abstract_dynamics(initial_state,z)
-	print(sT_mean.size())
-	print(sT_sig.size())
+	print(sT_mean.shape)
+	print(sT_sig.shape)
 	
 
 # 	# infer the skill
