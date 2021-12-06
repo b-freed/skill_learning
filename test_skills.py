12993a421aef704e3e8f58c5d9d84daaf5b22d58
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-12-06 06:59:16 +0530

    changed save file

--- test_skills.py
+++ test_skills.py
@@ -142,7 +142,7 @@ for i in range(episodes):
 
 plt.legend(['Actual Trajectory','Initial State','Predicted Terminal State'])
 plt.title('Skill Execution & Prediction (Skill-Dependent Prior)')
-plt.savefig('Skill_Prediction.png')
+plt.savefig('Skill_Prediction_H'+str(H)+'.png')
 
 	
 # 	# plt.scatter(states_actual[:,0],states_actual[:,1])
