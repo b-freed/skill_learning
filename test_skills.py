1ce9024771255cc324b65c121b7f5423511d0b1f
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2021-12-08 21:39:07 +0530

    Update test_skills.py

--- test_skills.py
+++ test_skills.py
@@ -51,7 +51,7 @@ def run_skill_with_disturbance(skill_model,s0,skill,env,H):
 if __name__ == '__main__':
 
 	device = torch.device('cuda:0')
-	H = 60
+	H = 80
 	state_dim = 4
 	a_dim = 2
 	h_dim = 128
