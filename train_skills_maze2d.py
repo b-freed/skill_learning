d168f127a9dede2d6f0e22c9c88706d4fb0a605c
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-01-07 00:23:17 +0530

    debugging

--- train_skills_maze2d.py
+++ train_skills_maze2d.py
@@ -19,7 +19,7 @@ def train(model,model_optimizer):
 	a_losses = []
 	kl_losses = []
 
-	for batch_id, data in enumerate(train_loader):
+	for batch_id, (data) in enumerate(train_loader):
 		data = data.cuda()
 		states = data[:,:,:model.state_dim]  # first state_dim elements are the state
 		actions = data[:,:,model.state_dim:]	 # rest are actions
@@ -47,7 +47,7 @@ def test(model):
 	kl_losses = []
 
 	with torch.no_grad():
-		for batch_id, data in enumerate(test_loader):
+		for batch_id, (data) in enumerate(test_loader):
 			data = data.cuda()
 			states = data[:,:,:model.state_dim]  # first state_dim elements are the state
 			actions = data[:,:,model.state_dim:]	 # rest are actions
