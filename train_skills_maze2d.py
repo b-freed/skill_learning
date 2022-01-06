9f811f83062ee05815469db94de99d11ae01a16e
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-01-06 23:56:12 +0530

    debugging

--- train_skills_maze2d.py
+++ train_skills_maze2d.py
@@ -154,7 +154,8 @@ experiment.log_parameters({'lr':lr,
 inputs = np.concatenate([obs_chunks, action_chunks],axis=-1) # array that is dataset_size x T x state_dim+action_dim
 
 train_data = TensorDataset(torch.tensor(inputs, dtype=torch.float32))
-train_data, test_data = torch.utils.data.random_split(train_data, [int(0.8*len(train_data)), int(0.2*len(train_data))])
+dataset_size = train_data.shape[0]
+train_data, test_data = torch.utils.data.random_split(train_data, [int(0.8*dataset_size), int(0.2*dataset_size)])
 
 train_loader = DataLoader(
 	train_data,
