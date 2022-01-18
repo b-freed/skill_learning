e8cec4f69f133e9788d6894f448bbbe072097fd1
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-01-19 04:30:24 +0530

    added more points to plot

--- test_skills.py
+++ test_skills.py
@@ -161,16 +161,6 @@ actual_states = np.stack(actual_states)
 pred_states_sig = np.stack(pred_states_sig)
 pred_states_mean = np.stack(pred_states_mean)
 
-'''
-plt.figure()
-ax = plt.gca()
-
-ellipse = Ellipse(xy=(pred_states_mean[:,0][0], pred_states_mean[:,1][0]), width=pred_states_sig[:,0][0], height=pred_states_sig[:,1][0], 
-                        edgecolor='r')
-ax.add_patch(ellipse)
-plt.title('Ellipse of std. dev of pred terminal states')
-'''
-
 # x = u + a cos(t) ; y = v + b sin(t)
 
 u = pred_states_mean[:,0][0]       #x-position of the center
@@ -179,12 +169,18 @@ a = (pred_states_sig[:,0][0])/2    #radius on the x-axis
 b = (pred_states_sig[:,1][0])/2    #radius on the y-axis
 
 t = np.linspace(0, 2*pi, 100)
+plt.figure()
 plt.plot( u+a*np.cos(t) , v+b*np.sin(t) )
 plt.scatter(u,v)
 plt.grid(color='lightgray',linestyle='--')
-plt.legend(['Std dev of Predicted terminal states', 'Mean of Predicted terminal states'])
-plt.title('Ellipse of std. dev of pred terminal states')
-plt.savefig('Confidence_Ellipse_H'+str(H)+'.png')
+
+plt.scatter(actual_states[:,:,0],actual_states[:,:,1], c='r')
+plt.scatter(actual_states[:,0,0],actual_states[:,0,1], c='b')
+plt.scatter(pred_states[:,0],pred_states[:,1], c='g')
+
+plt.legend(['Std dev of Predicted terminal states', 'Mean of Predicted terminal states','Actual Trajectory','Initial State','Predicted Terminal State'])
+plt.title('Skill Execution & Prediction (Skill-Dependent Prior)')
+plt.savefig('Skill_Prediction_H'+str(H)+'.png')
 
 #ipdb.set_trace()
 #plt.figure()
