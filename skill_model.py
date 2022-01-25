760597ad0846847bdafc7f3968147e05f9e89220
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-01-26 02:11:46 +0530

    debugging

--- skill_model.py
+++ skill_model.py
@@ -103,7 +103,7 @@ class LowLevelPolicy(nn.Module):
             action = nn.Tanh()(action)
         action = action.detach().cpu().numpy()
         
-        return action.reshape([2,])
+        return action.reshape([action.shape[1],])
      
     def reparameterize(self, mean, std):
         eps = torch.normal(torch.zeros(mean.size()).cuda(), torch.ones(mean.size()).cuda())
