b07ddda4c3710a90b95675882a81fefebe10f9d5
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-15 03:25:55 +0530

    changed imported libraries

--- mppi.py
+++ mppi.py
@@ -1,5 +1,27 @@
+from tokenize import ContStr
+from comet_ml import Experiment
+import numpy as np
 import torch
-from torch.distributions import Normal
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataloader import DataLoader
+import torch.distributions.normal as Normal
+from skill_model import SkillModel, SkillModelStateDependentPrior
+import ipdb
+import d4rl
+import random
+import gym
+from mujoco_py import GlfwContext
+GlfwContext(offscreen=True)
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+from math import pi
+
+device = torch.device('cuda:0')
 
 class MPPI(object):
 
