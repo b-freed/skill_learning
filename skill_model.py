fix bilevel v2 model and add some visualizations for variance

skill_model.py
@@ -125,7 +125,7 @@ class LowLevelDynamicsFF(nn.Module):
 
     def __init__(self,s_dim,a_dim,h_dim):
 
-        super(LowLevelDynamics,self).__init__()
+        super(LowLevelDynamicsFF,self).__init__()
         
         self.layer1 = nn.Sequential(nn.Linear(s_dim+a_dim,h_dim),nn.ReLU())
         self.mean_layer = nn.Sequential(nn.Linear(h_dim,h_dim),nn.ReLU(),nn.Linear(h_dim,s_dim))
@@ -144,11 +144,11 @@ class LowLevelDynamicsFF(nn.Module):
             s_next_sig:  batch_size x T x a_dim tensor of standard devs for predicted next states
         '''
 
-        state_acitons = torch.cat([states,actions],dim=-1)
+        state_actions = torch.cat([states,actions],dim=-1)
         feats = self.layer1(state_actions)
-        delta_means = self.mean_layer(feat)
+        delta_means = self.mean_layer(feats)
         s_next_mean = delta_means + states
-        s_next_sig = self.sig_layer(feat)
+        s_next_sig = self.sig_layer(feats)
 
 
 
@@ -587,6 +587,8 @@ class DecoderWithLLDynamics(nn.Module):
         else:
             self.ll_dynamics = LowLevelDynamics(state_dim,a_dim,h_dim)
         self.ll_policy = LowLevelPolicy(state_dim,a_dim,z_dim,h_dim, 'normal')
+
+        self.abstract = AbstractDynamics(state_dim, z_dim, h_dim)
         
     def forward(self,states,actions,z):
 
@@ -603,11 +605,12 @@ class DecoderWithLLDynamics(nn.Module):
         
         s_0 = states[:,0:1,:]
         z_detached = z.detach()
-        s_next_mean,s_next_sig,_ = self.ll_dynamics(states[:,:-1,:],actions[:,:-1,:])
+        sT_mean, sT_sig = self.abstract.forward(s_0, z)
+
         a_mean,a_sig   = self.ll_policy(states,z)
 
 
-        return s_next_mean,s_next_sig,a_mean,a_sig
+        return sT_mean,sT_sig,a_mean,a_sig
 
     def get_expected_sT_dist(self,s0,z,H,n_samples=10):
 
@@ -639,9 +642,9 @@ class DecoderWithLLDynamics(nn.Module):
 
         return sT_dist
 
-    def get_expected_next_state_dist(self,states,z):
+    def get_expected_next_state_dist(self,states,z,n_samples=10):
         assert self.ff
-        batch_size,T,state_dim = s0.shape
+        batch_size,T,state_dim = states.shape
         _,_,z_dim = z.shape
         states_tiled = torch.stack(n_samples*[states],dim=0)
         z_tiled      = torch.stack(n_samples*[z],dim=0)
@@ -651,9 +654,9 @@ class DecoderWithLLDynamics(nn.Module):
         a_means,a_sigs = self.ll_policy(states,z)
         a_means_tiled = torch.stack(n_samples*[a_means],dim=0) 
         a_sigs_tiled = torch.stack(n_samples*[a_sigs],dim=0)
-        actions = reparameterize(a_means,a_sigs)  # this should be n_samples distinct action sample for every (state,z)
+        actions = reparameterize(a_means_tiled,a_sigs_tiled)  # this should be n_samples distinct action sample for every (state,z)
 
-        next_s_means,next_s_sigs = self.ll_dynamics(states_tiled[:,:,:-1,:],actions[:,:,:-1,:])
+        s_next_means,s_next_sigs = self.ll_dynamics(states_tiled[:,:,:-1,:],actions[:,:,:-1,:])
         s_next_dist = GaussianMixtureDist(s_next_means,s_next_sigs)
 
         return s_next_dist
@@ -806,7 +809,7 @@ class BilevelSkillModelV2(nn.Module):
         a_dist = Normal.Normal(a_means, a_sigs)
         s_next_dist = Normal.Normal(s_next_means,s_next_sigs)
         
-        exp_s_next_dist = self.decoder.get_expected_next_state_dist(states,z_sampled,T)
+        exp_s_next_dist = self.decoder.get_expected_next_state_dist(states,z_sampled)
 
         s_next_loss = - torch.sum(exp_s_next_dist.log_prob(sT))/(batch_size*T)
         s_loss = - torch.sum(s_next_dist.log_prob(s_next))/(batch_size*T)
@@ -821,7 +824,7 @@ class BilevelSkillModelV2(nn.Module):
 
 
     def reparameterize(self, mean, std):
-        eps = torch.normal(torch.zeros(mean.size()).cuda(), torch.ones(mean.size()).cuda())
+        eps = torch.normal(torch.zeros_like(mean), torch.ones_like(mean))
         return mean + std*eps
     
 
@@ -938,3 +941,56 @@ class SkillModel(nn.Module):
         return mean + std*eps
 
 
+class StableSkillModel(nn.Module):
+    def __init__(self,state_dim,a_dim,z_dim,h_dim):
+        super(BilevelSkillModelV2, self).__init__()
+
+        self.state_dim = state_dim # state dimension
+        self.a_dim = a_dim # action dimension
+
+        self.encoder = Encoder(state_dim,a_dim,z_dim,h_dim)
+        self.decoder = DecoderWithLLDynamics(state_dim,a_dim,z_dim,h_dim,ff=True)
+        self.prior   = Prior(state_dim,z_dim,h_dim)
+
+    def forward(self):
+        pass
+
+    def get_losses(self,states,actions):
+
+
+        batch_size, T, _ = states.shape
+
+        s_next = states[:,1:,:]
+        s0 = states[:,:1,:]
+        sT = states[:,-1:,:]
+
+        z_post_means,z_post_sigs = self.encoder(states,actions)
+        z_post_dist = Normal.Normal(z_post_means, z_post_sigs) 
+        z_sampled = self.reparameterize(z_post_means,z_post_sigs)
+
+        
+        z_prior_means,z_prior_sigs = self.prior(s0) 
+        z_prior_dist = Normal.Normal(z_prior_means, z_prior_sigs) 
+
+        s_next_means,s_next_sigs,a_means,a_sigs = self.decoder(states,actions,z_sampled)
+
+        a_dist = Normal.Normal(a_means, a_sigs)
+        s_next_dist = Normal.Normal(s_next_means,s_next_sigs)
+        
+        exp_s_next_dist = self.decoder.get_expected_next_state_dist(states,z_sampled)
+
+        s_next_loss = - torch.sum(exp_s_next_dist.log_prob(sT))/(batch_size*T)
+        s_loss = - torch.sum(s_next_dist.log_prob(s_next))/(batch_size*T)
+        a_loss = - torch.sum(a_dist.log_prob(actions))/(batch_size*T)
+
+        kl_loss = torch.sum(KL.kl_divergence(z_post_dist, z_prior_dist))/(batch_size*T) # divide by T because all other losses we take mean over T dimension, effectively dividing by T
+
+        loss = s_loss + a_loss + s_next_loss + kl_loss
+
+        return loss, s_loss, a_loss, s_next_loss, kl_loss
+
+
+
+    def reparameterize(self, mean, std):
+        eps = torch.normal(torch.zeros_like(mean), torch.ones_like(mean))
+        return mean + std*eps

test_skills_bilevel.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset
 from torch.utils.data.dataloader import DataLoader
 import torch.distributions.normal as Normal
-from skill_model import SkillModel, SkillModelStateDependentPrior, BilevelSkillModel
+from skill_model import SkillModel, SkillModelStateDependentPrior, BilevelSkillModelV2
 import ipdb
 import d4rl
 import gym
@@ -62,7 +62,7 @@ if __name__ == '__main__':
 
 	device = torch.device('cuda:0')
 	
-	env = 'antmaze-medium-diverse-v0'
+	env = 'maze2d-large-dense-v1'
 	# env = 'kitchen-partial-v0'
 	env = gym.make(env)
 	data = env.get_dataset()
@@ -73,10 +73,10 @@ if __name__ == '__main__':
 	h_dim = 256
 	z_dim = 256
 	batch_size = 1
-	episodes = 20
+	episodes = 10
 	wd = .0
 	state_dependent_prior = True
-	n_skills = 3
+	n_skills = 2
 	colors = ['r','g','b']
 
 
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 	# # filename = 'Franka_H'+str(H)+'_l2reg_'+str(wd)+'_log_best.pth'
 	# PATH = 'checkpoints/'+filename
 	
-	filename = 'AntMaze_bilevel_H20_l2reg_0.0_log_best.pth'
+	filename = 'Maze2d_bilevel_H20_l2reg_0.0_log_best.pth'
 	PATH = 'checkpoints/'+filename
 
 
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 	# else:
 	# 	skill_model_sdp = SkillModelStateDependentPrior(state_dim, a_dim, z_dim, h_dim).cuda()
 
-	skill_model_sdp = BilevelSkillModel(state_dim,a_dim,z_dim,h_dim).cuda()
+	skill_model_sdp = BilevelSkillModelV2(state_dim,a_dim,z_dim,h_dim).cuda()
 
 		
 	checkpoint = torch.load(PATH)
@@ -213,20 +213,20 @@ if __name__ == '__main__':
 		state = torch.reshape(torch.tensor(state,dtype=torch.float32).cuda(), (1,1,state_dim))
 		#actions = torch.tensor(actions,dtype=torch.float32).cuda()
 
-		if not state_dependent_prior:
-			z_mean = torch.zeros((1,1,z_dim), device=device)
-			z_sig = torch.ones((1,1,z_dim), device=device)
-		else:
-			z_mean,z_sig = skill_model_sdp.prior(state)
-
-		z = skill_model_sdp.reparameterize(z_mean,z_sig)
+		
 
 		for i in range(episodes):
-			initial_state = env.reset()
-			state = initial_state
+			state = torch.reshape(torch.tensor(state,dtype=torch.float32).cuda(), (1,1,state_dim))
 			print('i: ', i)
 
-			state = torch.reshape(torch.tensor(state,dtype=torch.float32).cuda(), (1,1,state_dim))
+			if not state_dependent_prior:
+				z_mean = torch.zeros((1,1,z_dim), device=device)
+				z_sig = torch.ones((1,1,z_dim), device=device)
+			else:
+				z_mean,z_sig = skill_model_sdp.prior(state)
+
+			z = skill_model_sdp.reparameterize(z_mean,z_sig)
+
 			#actions = torch.tensor(actions,dtype=torch.float32).cuda()
 			
 			# if not state_dependent_prior:
@@ -257,8 +257,9 @@ if __name__ == '__main__':
 			# states_actual,actions = run_skill_with_disturbance(skill_model_sdp, states[:,0:1,:],z,env,H)
 			
 			
-			plt.scatter(states_actual[:,0],states_actual[:,1],c=colors[j])
-			plt.scatter(states_actual[0,0],states_actual[0,1],c=colors[j])
+			plt.scatter(states_actual[:,0],states_actual[:,1],color=colors[0])
+			plt.scatter(states_actual[0,0],states_actual[0,1],color=colors[1])
+			plt.scatter(states_actual[-1,0],states_actual[-1,1],color=colors[2])
 
 		actual_states.append(states_actual)
 		action_dist.append(actions)

utils.py
@@ -58,7 +58,7 @@ def make_video(frames,name):
     writer.close()
 
 def reparameterize(mean, std):
-    eps = torch.normal(torch.zeros(mean.size()).cuda(), torch.ones(mean.size()).cuda())
+    eps = torch.normal(torch.zeros_like(mean), torch.ones_like(mean))
     return mean + std*eps
 
 def stable_weighted_log_sum_exp(x,w,sum_dim):