7ae32bb4093681eb95d302a389c17b43a7cc5c8d
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-15 22:43:51 +0530

    working on mppi code

--- mppi.py
+++ mppi.py
@@ -24,62 +24,47 @@ from math import pi
 device = torch.device('cuda:0')
 
 
-class MPPI(object):
+def mppi_update(self, state, model, env, T, N, lam=0.1, eps=0.2):
+	a = torch.zeros(T, model.a_dim).to(device)
+	eps = Normal(torch.zeros(N, model.a_dim).to(device),
+                            (torch.ones(N, model.a_dim)*eps).to(device))
 
-    def __init__(self, model, samples=40, T=10, lam=0.1, eps=0.2):
+	with torch.no_grad():
+		a[:-1] = a[1:].clone()
+		a[-1].zero_()
 
-        self.model          = model
-        self.num_actions    = model.num_actions
-        self.t_H            = T
-        self.lam            = lam
-        self.samples        = samples
+		s0 = torch.FloatTensor(state).unsqueeze(0).to(device)
+		s = s0.repeat(N, 1)
 
-        self.device = device
+		sk, da, log_prob = [], [], []
+		eta = None
+		gamma = 0.5
+	    	for t in range(T):
+			eps = eps.sample()
+			eta = eps
+			# if eta is None:
+			#     eta = eps
+			# else:
+			#     eta = gamma*eta + ((1-gamma**2)**0.5) * eps
+			v = a[t].expand_as(eta) + eta
+			s, rew,_,_ = env.step(v)
+			log_prob.append(eps.log_prob(eta).sum(1))
+			da.append(eta)
+			sk.append(rew.squeeze())
 
-        self.a = torch.zeros(T, self.num_actions).to(self.device)
-        self.eps = Normal(torch.zeros(self.samples, self.num_actions).to(self.device),
-                            (torch.ones(self.samples, self.num_actions)*eps).to(self.device))
+		sk = torch.stack(sk)
+		sk = torch.cumsum(sk.flip(0), 0).flip(0)
+		log_prob = torch.stack(log_prob)
 
-    def reset(self):
-        self.a.zero_()
-
-    def update(self, state):
-
-        with torch.no_grad():
-            self.a[:-1] = self.a[1:].clone()
-            self.a[-1].zero_()
-
-            s0 = torch.FloatTensor(state).unsqueeze(0).to(self.device)
-            s = s0.repeat(self.samples, 1)
-
-            sk, da, log_prob = [], [], []
-            eta = None
-            gamma = 0.5
-            for t in range(self.t_H):
-                eps = self.eps.sample()
-                eta = eps
-                # if eta is None:
-                #     eta = eps
-                # else:
-                #     eta = gamma*eta + ((1-gamma**2)**0.5) * eps
-                v = self.a[t].expand_as(eta) + eta
-                s, rew = self.model.step(s, v)
-                log_prob.append(self.eps.log_prob(eta).sum(1))
-                da.append(eta)
-                sk.append(rew.squeeze())
-
-            sk = torch.stack(sk)
-            sk = torch.cumsum(sk.flip(0), 0).flip(0)
-            log_prob = torch.stack(log_prob)
-
-            sk = sk + self.lam*log_prob
-            sk = sk - torch.max(sk, dim=1, keepdim=True)[0]
-            w = torch.exp(sk.div(self.lam)) + 1e-5
-            w.div_(torch.sum(w, dim=1, keepdim=True))
-            for t in range(self.t_H):
-                self.a[t] = self.a[t] + torch.mv(da[t].T, w[t])
-
-            return self.a[0].cpu().clone().numpy()
+		sk = sk + lam*log_prob
+		sk = sk - torch.max(sk, dim=1, keepdim=True)[0]
+		w = torch.exp(sk.div(lam)) + 1e-5
+		w.div_(torch.sum(w, dim=1, keepdim=True))
+	    	for t in range(T):
+			a[t] = a[t] + torch.mv(da[t].T, w[t])
+	
+		
+		return a[0].reshape([model.a_dim,]).cpu().clone().numpy()
 	
 			
 			
