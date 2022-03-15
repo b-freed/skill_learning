c6aa9d17ec6b23db66213e308477aa0bc7075326
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-15 21:24:37 +0530

    working on mppi code

--- mppi.py
+++ mppi.py
@@ -24,98 +24,63 @@ from math import pi
 device = torch.device('cuda:0')
 
 
-def mppi_iter_variable_length(x,cost_fn,l2_pen,eps,samples,z):
+class MPPI(object):
 
-		
-		
-		
-		
-		
-		
-		
-		
-	
+    def __init__(self, model, samples=40, T=10, lam=0.1, eps=0.2):
 
-def mppi(x_mean,x_std,cost_fn,pop_size,n_iters,samples=40,eps=0.2,max_ep=None):
-	
-	max_length = x_mean.shape[1]
-	z = torch.zeros_like(x_mean).to(device)
-	eps = Normal(torch.zeros(samples, x_mean.shape[0]).to(device),
-                            (torch.ones(samples, x_mean.shape[0])*eps).to(device))
+        self.model          = model
+        self.num_actions    = model.num_actions
+        self.t_H            = T
+        self.lam            = lam
+        self.samples        = samples
 
+        self.device = device
+
+        self.a = torch.zeros(T, self.num_actions).to(self.device)
+        self.eps = Normal(torch.zeros(self.samples, self.num_actions).to(self.device),
+                            (torch.ones(self.samples, self.num_actions)*eps).to(self.device))
+
+    def reset(self):
+        self.a.zero_()
+
+    def update(self, state):
+
+        with torch.no_grad():
+            self.a[:-1] = self.a[1:].clone()
+            self.a[-1].zero_()
+
+            s0 = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+            s = s0.repeat(self.samples, 1)
+
+            sk, da, log_prob = [], [], []
+            eta = None
+            gamma = 0.5
+            for t in range(self.t_H):
+                eps = self.eps.sample()
+                eta = eps
+                # if eta is None:
+                #     eta = eps
+                # else:
+                #     eta = gamma*eta + ((1-gamma**2)**0.5) * eps
+                v = self.a[t].expand_as(eta) + eta
+                s, rew = self.model.step(s, v)
+                log_prob.append(self.eps.log_prob(eta).sum(1))
+                da.append(eta)
+                sk.append(rew.squeeze())
+
+            sk = torch.stack(sk)
+            sk = torch.cumsum(sk.flip(0), 0).flip(0)
+            log_prob = torch.stack(log_prob)
+
+            sk = sk + self.lam*log_prob
+            sk = sk - torch.max(sk, dim=1, keepdim=True)[0]
+            w = torch.exp(sk.div(self.lam)) + 1e-5
+            w.div_(torch.sum(w, dim=1, keepdim=True))
+            for t in range(self.t_H):
+                self.a[t] = self.a[t] + torch.mv(da[t].T, w[t])
+
+            return self.a[0].cpu().clone().numpy()
 	
-	with torch.no_grad():
-		z[:-1] = z[1:].clone()
-		z[-1].zero_()
-		sk, da, log_prob = [], [], []
-		eta = None
-		gamma = 0.5
-		for i in range(n_iters):
-			eps = self.eps.sample()
-			eta = eps
-			v = z[t].expand_as(eta) + eta
-			x = cost_fn(x_mean,v)
-			log_prob.append(eps.log_prob(eta).sum(1))
-			da.append(eta)
-			
-	
-	return x_mean,x_std
-	
-	
 			
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		
     
