836e500230a53efc6ce4e355d68392f9c5f6ed90
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-15 10:18:56 +0530

    working on mppi code

--- mppi.py
+++ mppi.py
@@ -23,11 +23,43 @@ from math import pi
 
 device = torch.device('cuda:0')
 
-def mppi_iter_variable_length(x_mean,x_std,p_lengths,cost_fn,pop_size,frac_keep,n_iters,lam,max_ep=None,l2_pen=0):
-	
-	pass
 
-def mppi_variable_length()
+def mppi_iter_variable_length(x,cost_fn,l2_pen,eps,samples,z):
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+def mppi(x_mean,x_std,cost_fn,pop_size,n_iters,samples=40,eps=0.2,max_ep=None):
+	
+	max_length = x_mean.shape[1]
+	z = torch.zeros_like(x_mean).to(device)
+	eps = Normal(torch.zeros(samples, x_mean.shape[0]).to(device),
+                            (torch.ones(samples, x_mean.shape[0])*eps).to(device))
+
+	
+	with torch.no_grad():
+		z[:-1] = z[1:].clone()
+		z[-1].zero_()
+		sk, da, log_prob = [], [], []
+		eta = None
+		gamma = 0.5
+		for i in range(n_iters):
+			eps = self.eps.sample()
+			eta = eps
+			v = z[t].expand_as(eta) + eta
+			x = cost_fn(x_mean,v)
+			log_prob.append(eps.log_prob(eta).sum(1))
+			da.append(eta)
+			
+	
+	return x_mean,x_std
 	
 	
 			
