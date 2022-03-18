e341fff7abc032660a5c3c0cee0a395fea907d02
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-18 22:39:14 +0530

    Update mppi.py

--- mppi.py
+++ mppi.py
@@ -24,7 +24,7 @@ from math import pi
 device = torch.device('cuda:0')
 
 
-def mppi_update(skill_seq_mean, s0, model, cost_fn, batch_size, T, z_dim, lam=0.1, eps=0.2):
+def mppi_update(skill_seq_mean, s0, model, cost_fn, batch_size, T, z_dim, plot, goal_state, lam=0.1, eps=0.2):
 
 	eps_dist = Normal(torch.zeros((batch_size, z_dim),device=device),torch.ones((batch_size, z_dim),device=device)*eps)
 
@@ -36,7 +36,7 @@ def mppi_update(skill_seq_mean, s0, model, cost_fn, batch_size, T, z_dim, lam=0.
 		#skill_seq_mean = skill_seq_mean.tile([batch_size,1,1])
 		s = s0.repeat(batch_size, 1)
 
-		sk, dz, log_prob = [], [], []
+		sk, dz, log_prob, pred_states = [], [], [], [s0]
 		eta = None
 		gamma = 0.5
 		for t in range(T):
@@ -48,10 +48,12 @@ def mppi_update(skill_seq_mean, s0, model, cost_fn, batch_size, T, z_dim, lam=0.
 			#     eta = gamma*eta + ((1-gamma**2)**0.5) * eps
 			v = skill_seq_mean[t].expand_as(eta) + eta
 			s,_ = model.decoder.abstract_dynamics(s,v)
+			s_i = s
 			rew = cost_fn(s)
 			log_prob.append(eps_dist.log_prob(eta).sum(1))
 			dz.append(eta)
 			sk.append(rew.squeeze())
+			pred_states.append(s_i)
 
 		sk = torch.stack(sk)
 		sk = torch.cumsum(sk.flip(0), 0).flip(0)
@@ -64,5 +66,18 @@ def mppi_update(skill_seq_mean, s0, model, cost_fn, batch_size, T, z_dim, lam=0.
 		for t in range(T):
 			skill_seq_mean[t] = skill_seq_mean[t] + torch.mv(dz[t].T, w[t])
 
+		goal_state = torch.cat(batch_size * [goal_state],dim=0)
+		pred_states = torch.stack(pred_states, dim=1)
+		if plot:
+			plt.figure()
+			plt.scatter(s0[0].detach().cpu().numpy(),s0[1].detach().cpu().numpy(), label='init state')
+			plt.scatter(goal_state[0,0,0].detach().cpu().numpy(),goal_state[0,0,1].detach().cpu().numpy(),label='goal',s=300)
+			# plt.xlim([0,25])
+			# plt.ylim([0,25])
+			#pred_states = torch.cat(pred_states,1)
+
+			plt.plot(pred_states[:,:,0].T.detach().cpu().numpy(),pred_states[:,:,1].T.detach().cpu().numpy())
+				
+			plt.savefig('pred_states_cem')
+
 		return skill_seq_mean
-    
