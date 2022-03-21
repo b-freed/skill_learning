5a6f23a434ab963a8e91f2858ebb270ab4b804d2
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-03-21 19:28:36 +0530

    Update plan_skills_mppi.py

--- plan_skills_mppi.py
+++ plan_skills_mppi.py
@@ -21,7 +21,7 @@ from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
 from math import pi
 from mppi import mppi_update
-from utils import make_gif
+from utils import make_gif, make_video
 import cv2
 
 device = torch.device('cuda:0')
@@ -38,7 +38,7 @@ a_dim = data['actions'].shape[1]
 h_dim = 256
 z_dim = 256
 batch_size = 100
-skill_seq_len = 20
+skill_seq_len = 100
 lr = 1e-4
 wd = 0.001
 state_dependent_prior = True
@@ -47,9 +47,13 @@ beta = 1.0
 alpha = 1.0
 max_sig = None
 fixed_sig = None
+use_epsilon = True
 ent_pen = 0.0
-n_iters = 50
+n_iters = 100
 a_dist = 'normal'
+render = False
+max_replans = 50
+
 # background_img = mpimg.imread('maze_medium.png')
 
 
@@ -107,6 +111,116 @@ goal_seq = torch.tensor(goal_state, device=device).reshape(1,1,-1)
 # 			  				   'state_dim':state_dim,
 # 			  				   'l2_reg':wd})
 #experiment.log_metric('Goals', goal_seq)
+
+def run_skills_iterative_replanning(env,model,goals,use_epsilon,replan_freq,ep_num):
+	
+	s0 = env.reset()
+	state = s0
+	plt.scatter(s0[0],s0[1], label='Initial States')
+	plt.scatter(goals[:,:,0].detach().cpu().numpy(),goals[:,:,1].detach().cpu().numpy(), label='Goals')
+	plt.figure()
+	# for i in range(skill_seq_len):
+	# ipdb.set_trace()
+	states = [s0]
+	frames = []
+	n=0
+	timeout = False
+	# success = True
+	l = skill_seq_len
+	while np.sum((state[:2] - goals.flatten().detach().cpu().numpy()[:2])**2) > 1.0:
+	# for i in range(2):
+		state_torch = torch.tensor(state,dtype=torch.float32).cuda()
+		cost_fn = lambda s: skill_model.cost_for_mppi(s, goal_seq)
+		skill_seq_mean = torch.zeros((skill_seq_len,z_dim),device=device)
+		skill_seq_std  = torch.ones( (skill_seq_len,z_dim),device=device)
+
+
+		skill_seq_mean = mppi_update(skill_seq_mean, state_torch, skill_model, cost_fn, batch_size, skill_seq_len, z_dim, True, goal_seq)
+
+
+
+		if skill_seq_mean.shape[0] == 0:
+
+			print('OUT OF SKILLS!!!')
+			# out_of_skills = True
+			# break
+		else:
+			skill = skill_seq_mean[0,:].unsqueeze(0)
+
+			
+		if use_epsilon:
+			mu_z, sigma_z = model.prior(torch.tensor(state,dtype=torch.float32).cuda().reshape(1,1,-1))
+			z = mu_z + sigma_z*skill
+		else:
+			z = skill
+		for j in range(replan_freq):
+		# for j in range(100):
+			if render:
+				frames.append(env.render(mode='rgb_array'))
+			# env.render()
+			# vid.capture_frame()
+			action = model.decoder.ll_policy.numpy_policy(state,z)
+			state,_,done,_ = env.step(action)
+			# print('state: ', state)
+			states.append(state)
+			
+			# skill_seq_states.append(state)
+			# plt.scatter(state[0],state[1], label='Trajectory',c='b')
+			if np.sum((state[:2] - goals.flatten().detach().cpu().numpy()[:2])**2) <= 1.0:
+				break
+			if done:
+				print('DOOOOOOOOONE!!!!!!!!!!!!!!')
+				print('state: ', state)
+			# 	print('n: ',n)
+				# break
+		n += 1
+
+		
+	
+
+		fig = plt.figure()
+		# plt.imshow(background_img, extent = [-8,28,-8,28])
+		plt.scatter(np.stack(states)[:,0],np.stack(states)[:,1])
+		plt.scatter(goals.flatten().detach().cpu().numpy()[0],goals.flatten().detach().cpu().numpy()[1])
+		plt.axis('equal')
+		plt.savefig('ant_iterative_replanning_actual_states_niters'+str(n_iters)+'.png')
+	
+		if n > max_replans*H/replan_freq:
+			print('TIMEOUT!!!!!!!!!!!!!!')
+			timeout = True
+			break 
+	
+
+
+		# plt.savefig('ant_skills_iterative_replanning')
+	# ipdb.set_trace()
+
+
+	# save_frames_as_gif(frames)
+	# for i,f in enumerate(frames):
+	# 	plt.figure()
+	# 	plt.imshow(f)
+	# 	plt.savefig('ant'+str())
+	env.close()
+	# make_gif(frames,name='ant')
+	if render or timeout:
+		print('MAKING VIDEO!')
+		# if timeout: 
+		# 	print('making timout vid')
+		# 	make_video(frames,name='failed_ant_'+str(j))
+		# else:
+		# 	make_video(frames,name='ant')
+		make_video(frames,name='ant'+str(ep_num))
+		# make_video(frames,name='yant')
+		# make_video(frames,name='yant')
+
+
+	states = np.stack(states)
+	return states, np.min(np.sum((states[:,:2] - goals.flatten().detach().cpu().numpy()[:2])**2,axis=-1))
+
+
+
+
 
 def run_skill_seq(env,s0,model, skill_seq):
 	'''
@@ -173,11 +287,24 @@ def run_skill_seq(env,s0,model, skill_seq):
 
 	print('SAVED FIG!')
 
+'''
 
 cost_fn = lambda s: skill_model.cost_for_mppi(s, goal_seq)
 skill_seq_mean = torch.zeros((skill_seq_len,z_dim),device=device)
 skill_seq_std  = torch.ones( (skill_seq_len,z_dim),device=device)
-skill_seq = mppi_update(skill_seq_mean, s0_torch, skill_model, cost_fn, batch_size, skill_seq_len, z_dim, True, goal_seq)
+for i in range(n_iters):
+	skill_seq = mppi_update(skill_seq_mean, s0_torch, skill_model, cost_fn, batch_size, skill_seq_len, z_dim, True, goal_seq)
+	skill_seq_mean = skill_seq
 skill_seq = skill_seq.unsqueeze(0)
 #skill_seq = skill_seq.tile([batch_size,1,1])
 run_skill_seq(env,s0_torch,skill_model, skill_seq)
+
+'''
+
+min_dists_list = []
+for i in range(100):
+	states,min_dist = run_skills_iterative_replanning(env,skill_model,goal_seq,use_epsilon,replan_freq,i)
+	min_dists_list.append(min_dist)
+	print('min_dists_list: ',min_dists_list)
+	np.save('min_dists_list_n_iters_'+str(n_iters),min_dists_list)
+	print('----------------'+str(i)+'-----------------')
