f277e991cfbc1b39c3c8dcd710f8611614010ede
Anirudh B <45899888+Anirudh-27@users.noreply.github.com>
2022-04-09 09:24:11 +0530

    added new chunking fn for online training

--- utils.py
+++ utils.py
@@ -109,3 +109,38 @@ def chunks(obs,next_obs,actions,H,stride):
 			
 	
 	return torch.stack(obs_chunks),torch.stack(action_chunks)
+
+
+def chunks_online(obs,actions,H,stride):
+	'''
+	obs is a N x 4 array
+	goals is a N x 2 array
+	H is length of chunck
+	stride is how far we move between chunks.  So if stride=H, chunks are non-overlapping.  If stride < H, they overlap
+	'''
+	
+	obs_chunks = []
+	action_chunks = []
+	N = obs.shape[0]
+	for i in range(N//stride - H):
+		start_ind = i*stride
+		end_ind = start_ind + H
+		# If end_ind = 4000000, it goes out of bounds
+		# this way start_ind is from 0-3999980 and end_ind is from 20-3999999
+		# if end_ind == N:
+		# 	end_ind = N-1
+		
+		obs_chunk = torch.tensor(obs[start_ind:end_ind,:],dtype=torch.float32)
+
+		action_chunk = torch.tensor(actions[start_ind:end_ind,:],dtype=torch.float32)
+		
+		# print('obs[start_ind+1:end_ind]: ',obs[start_ind+1:end_ind])
+		# print('next_obs[start_ind:end_ind-1]: ', next_obs[start_ind:end_ind-1])
+		obs_chunks.append(obs_chunk)
+		action_chunks.append(action_chunk)
+
+	print('len(obs_chunks): ',len(obs_chunks))
+	print('len(action_chunks): ',len(action_chunks))
+			
+	
+	return torch.stack(obs_chunks),torch.stack(action_chunks)
