71c198abd3de0dcb91689d22510559ebe0169d52
b-freed <bfreed@andrew.cmu.edu>
2022-05-28 10:45:50 -0400

    debug two stage skill model

--- train_skills_two_stage.py
+++ train_skills_two_stage.py
@@ -1,42 +1,241 @@
+from comet_ml import Experiment
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataloader import DataLoader
+from torch.distributions.transformed_distribution import TransformedDistribution
+# import torch.distributions.normal as Normal
+# import torch.distributions.categorical as Categorical
+# import torch.distributions.mixture_same_family as MixtureSameFamily
+# import torch.distributions.kl as KL
+import gym
+from mujoco_py import GlfwContext
+GlfwContext(offscreen=True)
+import d4rl
+import ipdb
+import matplotlib.pyplot as plt
+from utils import chunks
+# from skill_model import LowLevelPolicy,Encoder,Prior,AbstractDynamics,GenerativeModel
+from two_stage_skill_model import TwoStageSkillModel
+import os
+from datetime import date
 
 
 
+def train(model,train_loader):
+	
+	if model.stage == 'train_skills':
+		losses, a_losses, kl_losses= [],[],[]
+		for batch_id, data in enumerate(train_loader):
+			loss,a_loss,kl_loss = model.train(data)
+			losses.append(loss.item())
+			a_losses.append(a_loss.item())
+			kl_losses.append(kl_loss.item())
+		return np.mean(losses),np.mean(a_losses),np.mean(kl_losses)
 
-def train():
-    
-    # loss_list_dict = 
-    # for 
-        # loss_dict = model.update(data,mode)
-        # append losses
-        # for k in loss_dict.keys():
+			
+	elif model.stage == 'train_dynamics':
+		E_losses,M_losses = [],[]
+		for batch_id, data in enumerate(train_loader):
+			E_loss,M_loss = model.train(data)
+			E_losses.append(E_loss.item())
+			M_losses.append(M_loss.item())
+		return np.mean(E_losses),np.mean(M_losses)
+ 
 
-    pass
+	else: 
+		assert False    
 
-def validate():
 
-    pass
+		 
 
-stage = # 'train_skills' or 'train_dynamics'
 
+def validate(model,test_loader):
+	
+	losses,like_losses,kl_losses = [],[],[]
+	for batch_id, data in enumerate(test_loader):
+		loss,like_loss,kl_loss = model.validate(data)
+		losses.append(loss.item())
+		like_losses.append(like_loss.item())
+		kl_losses.append(kl_loss.item())
+
+	return np.mean(losses),np.mean(like_losses),np.mean(kl_losses)
+
+stage = 'train_dynamics' # 'train_skills' # 
 assert stage in ['train_skills','train_dynamics']
 
-# make filenames for both
-skill_filename = 
-dynamics_filename = 
 
-skill_path = 
-dynamics_path = 
+env_name = 'kitchen-partial-v0'
+dataset_file = 'datasets/'+env_name+'.npz'
 
-# initialize action-generative model
-skill_model = SkillPolicyModel()  # TODO pass arguments
-dymamics_model =   # TODO
+batch_size = 100
+h_dim = 256
+z_dim = 256
+lr = 5e-5
+wd = 0.0
+skill_beta = 0.1
+model_beta = 1.0
+H = 40
+stride = 1
+n_epochs = 50000
+test_split = .2
+a_dist = 'autoregressive'#, 'tanh_normal' or 'normal'
+state_dependent_prior = True
+encoder_type = 'state_action_sequence' #'state_sequence'
+state_decoder_type = 'mlp' #'autoregressive'
+init_state_dependent = True
+fixed_sig = None
+per_element_sigma = False
 
-model = TwoStageSkillModel(skill_model,dynamics_model,mode)
+if dataset_file is None:
+	env = gym.make(env_name)
+	dataset = env.get_dataset()
+else:
+	if '.npz' in dataset_file:
+		# load numpy file
+		dataset = np.load(dataset_file)
+	else:
+		raise NotImplementedError
+
+states = dataset['observations']
+actions = dataset['actions']
+
+N = states.shape[0]
+
+state_dim = states.shape[1]
+a_dim = actions.shape[1]
+
+N_train = int((1-test_split)*N)
+N_test = N - N_train
+
+states_train  = states[:N_train,:]
+actions_train = actions[:N_train,:]
 
 
-if stage == 'train_dynamics':
-    # load the skill model from checkpoint
-    skill_model.load_skills_from_ckpt(skill_path)
+states_test  = states[N_train:,:]
+actions_test = actions[N_train:,:]
+
+													#  obs,next_obs,actions,H,stride
+obs_chunks_train, action_chunks_train = chunks(states_train, actions_train, H, stride)
+
+print('states_test.shape: ',states_test.shape)
+print('MAKIN TEST SET!!!')
+
+obs_chunks_test,  action_chunks_test  = chunks(states_test,  actions_test,  H, stride)
+
+inputs_train = torch.cat([obs_chunks_train, action_chunks_train],dim=-1)
+inputs_test  = torch.cat([obs_chunks_test,  action_chunks_test], dim=-1)
+
+train_data = TensorDataset(inputs_train)
+test_data  = TensorDataset(inputs_test)
+
+train_loader = DataLoader(
+	inputs_train,
+	batch_size=batch_size,
+	num_workers=0)  # not really sure about num_workers...
+
+test_loader = DataLoader(
+	inputs_test,
+	batch_size=batch_size,
+	num_workers=0)
+
+# make path
+if stage == 'train_skills':
+	date =  date.today().strftime("%m_%d_%y")
+	path = 'checkpoints/two_stage_'+date+'_'+env_name+'_H_'+str(H)+'_l2reg_'+str(wd)+'_skill_b_'+str(skill_beta)+'_model_b_'+str(model_beta)+'_per_el_sig_'+str(per_element_sigma)+'_a_dist_'+str(a_dist) # TODO
+else:
+	path = 'checkpoints/two_stage_05_20_22_kitchen-partial-v0_H_40_l2reg_0.0_skill_b_0.1_model_b_1.0_per_el_sig_False_a_dist_autoregressive'
+
+assert path != None
+
+if not os.path.isdir(path):
+	os.mkdir(path)
+
+print('a_dim: ', a_dim)
+model = TwoStageSkillModel(state_dim,a_dim,z_dim,h_dim,a_dist,stage,path,skill_beta=skill_beta,model_beta=model_beta,fixed_sig=fixed_sig,per_element_sigma=per_element_sigma,lr=lr,wd=wd).cuda()
+
+experiment = Experiment(api_key = '9mxH2vYX20hn9laEr0KtHLjAa', project_name = 'skill-learning')
+experiment.log_parameters({'lr':lr,
+							'h_dim':h_dim,
+							'state_dependent_prior':state_dependent_prior,
+							'z_dim':z_dim,
+							'H':H,
+							'a_dim':a_dim,
+							'state_dim':state_dim,
+							'l2_reg':wd,
+							'skill_beta':skill_beta,
+							'model_beta':model_beta,
+							'env_name':env_name,
+							'path':path,
+							'encoder_type':encoder_type,
+							'state_decoder_type':state_decoder_type,
+							'per_element_sigma':per_element_sigma,
+							'a_dist':a_dist})
+
+# TODO get data
+
+min_test_loss = 10**10
+min_test_a_loss = 10**10
+min_test_sT_loss = 10**10
+for i in range(n_epochs):
+	if stage == 'train_skills':
+		# test
+		test_loss,test_a_loss,test_kl_loss = validate(model,test_loader)
+		# log test losses
+		experiment.log_metric("test_loss", test_loss, step=i)
+		experiment.log_metric("test_a_loss", test_a_loss, step=i)
+		experiment.log_metric("test_kl_loss", test_kl_loss, step=i)
+		print('test_loss: ', test_loss)
+		print('test_a_loss: ', test_a_loss)
+		print('test_kl_loss: ', test_kl_loss)
+		# decide whether to save a checkpoint
+		if test_loss < min_test_loss:
+			min_test_loss = test_loss
+			# save 
+			model.save_checkpoint('log_best.pth')
+
+		# train
+		loss,a_loss,kl_loss = train(model,train_loader)
+		# log train losses
+		experiment.log_metric("loss", loss, step=i)
+		experiment.log_metric("a_loss", a_loss, step=i)
+		experiment.log_metric("kl_loss", kl_loss, step=i)
+
+		print('loss: ', loss)
+		print('a_loss: ', a_loss)
+		print('kl_loss: ', kl_loss)
+		
+		
+		if i % 10 == 0:
+		   model.save_checkpoint('log.pth')
+
+		pass
+	elif stage == 'train_dynamics':
+		# test
+		test_loss,test_s_T_loss,test_kl_loss = validate(model,test_loader)
+		# log test losses
+		experiment.log_metric("test_loss", test_loss, step=i)
+		experiment.log_metric("test_s_T_loss", test_s_T_loss, step=i)
+		experiment.log_metric("test_kl_loss", test_kl_loss, step=i)
+		# decide whether to save a checkpoint
+		if test_loss < min_test_loss:
+			min_test_loss = test_loss
+			# save 
+			model.save_checkpoint('log_best.pth')
+		# train
+		E_loss,M_loss = train(model,train_loader)
+		# log train losses
+		experiment.log_metric("E_loss", E_loss, step=i)
+		experiment.log_metric("M_loss", M_loss, step=i)
+
+		if i % 10 == 0:
+			model.save_checkpoint('log.pth')
+	else:
+		assert False
+
+	
 
 
 
--- two_stage_skill_model.py
+++ two_stage_skill_model.py
@@ -12,70 +12,77 @@ import torch.distributions.kl as KL
 import ipdb
 import matplotlib.pyplot as plt
 from utils import reparameterize
-
-
+from skill_model import LowLevelPolicy, AutoregressiveLowLevelPolicy,Encoder,Prior,AbstractDynamics,GenerativeModel
+import os
 
 
 
 class SkillPolicyModel(nn.Module):
 
-    def __init__(self,state_dim,a_dim,z_dim,h_dim,beta,a_dist,fixed_sig=None):
-    	super().__init__()
-		self.ll_policy = LowLevelPolicy(state_dim,a_dim,z_dim,h_dim,a_dist=a_dist,max_sig=None,fixed_sig=fixed_sig)
-        self.encoder = Encoder(state_dim,a_dim,z_dim,h_dim)
-        self.prior = Prior(state_dim,z_dim,h_dim)
-        self.beta = beta
+	def __init__(self,state_dim,a_dim,z_dim,h_dim,beta,a_dist,fixed_sig=None):
+		super().__init__()
+		if a_dist == 'autoregressive':
+			self.ll_policy = AutoregressiveLowLevelPolicy(state_dim,a_dim,z_dim,h_dim,max_sig=None,fixed_sig=fixed_sig)
+		else:
+			self.ll_policy = LowLevelPolicy(state_dim,a_dim,z_dim,h_dim,a_dist=a_dist,max_sig=None,fixed_sig=fixed_sig)
+		self.encoder = Encoder(state_dim,a_dim,z_dim,h_dim)
+		self.prior = Prior(state_dim,z_dim,h_dim)
+		self.beta = beta
+		self.a_dist = a_dist
 
-    def get_losses(self,states,actions):
-        # STEP 1. Encode states and actions to get posterior over z
+	def get_losses(self,states,actions):
+		T = states.shape[1]
+		# STEP 1. Encode states and actions to get posterior over z
 		z_post_means,z_post_sigs = self.encoder(states,actions)
-      	z_post_dist = Normal.Normal(z_post_means, z_post_sigs)
+		z_post_dist = Normal.Normal(z_post_means, z_post_sigs)
 
 		# STEP 2. sample z from posterior 
 		z_sampled = reparameterize(z_post_means,z_post_sigs)
 
 		# STEP 3. Pass z_sampled and states through policy
-        if self.a_dist != 'autoregressive': 
-		    a_means, a_sigs = self.ll_policy(states,z_sampled)
-        else:
-		    a_means, a_sigs = self.ll_policy(states,actions,z_sampled)
-        a_dist = Normal.Normal(a_means, a_sigs)
+		if self.a_dist != 'autoregressive': 
+			a_means, a_sigs = self.ll_policy(states,z_sampled)
+		else:
+			a_means, a_sigs = self.ll_policy(states,actions,z_sampled)
+		a_dist = Normal.Normal(a_means, a_sigs)
 
-        # STEP 4. Get prior.
-        z_prior_means,z_prior_sigs = self.prior(states[:,0:1,:]) 
+		# STEP 4. Get prior.
+		z_prior_means,z_prior_sigs = self.prior(states[:,0:1,:]) 
 		z_prior_dist = Normal.Normal(z_prior_means, z_prior_sigs) 
 
-    	# STEP 5. Get losses.
-        a_loss   = -torch.mean(torch.sum(a_dist.log_prob(actions), dim=-1)) 
+		# STEP 5. Get losses.
+		a_loss   = -torch.mean(torch.sum(a_dist.log_prob(actions), dim=-1)) 
 		kl_loss = torch.mean(torch.sum(KL.kl_divergence(z_post_dist, z_prior_dist), dim=-1))/T # divide by T because all other losses we take mean over T dimension, effectively dividing by T
-        loss = a_loss + self.beta*kl_loss
+		loss = a_loss + self.beta*kl_loss
 
 		return loss, a_loss, kl_loss 
 
 class DynamicsModel(nn.Module):
-    def __init__(self,state_dim,z_dim,h_dim,beta,per_element_sigma):
-        super().__init__()
-        self.dynamics_model = AbstractDynamics(state_dim,z_dim,h_dim,init_state_dependent=True,per_element_sigma=per_element_sigma)
-        self.encoder = Encoder(state_dim,a_dim,z_dim,h_dim)
-        self.prior = Prior(state_dim,z_dim,h_dim)
-        self.beta = beta
+	def __init__(self,state_dim,a_dim,z_dim,h_dim,beta,per_element_sigma):
+		super().__init__()
+		self.dynamics_model = AbstractDynamics(state_dim,z_dim,h_dim,init_state_dependent=True,per_element_sigma=per_element_sigma)
+		self.encoder = Encoder(state_dim,a_dim,z_dim,h_dim)
+		self.prior = Prior(state_dim,z_dim,h_dim)
+		self.beta = beta
+		self.gen_model = GenerativeModel(self.dynamics_model,self.prior) # this is just a container so we can easily access params we update in M step
 
 
-    def get_losses(self,states,actions):
-        s_0 = states[:,0:1,:]
+	def get_losses(self,states,actions):
+		s_0 = states[:,0:1,:]
 		s_T = states[:,-1:,:]
-        # STEP 1. Encode states and actions to get posterior over z
+		T = states.shape[1]
+		# STEP 1. Encode states and actions to get posterior over z
 		z_post_means,z_post_sigs = self.encoder(states,actions)
 		# STEP 2. sample z from posterior 
-		z_sampled = self.reparameterize(z_post_means,z_post_sigs)
+		z_sampled = reparameterize(z_post_means,z_post_sigs)
 		# STEP 3. Pass z_sampled and states through decoder 
-		s_T_mean, s_T_sig = self.dynamics_model(s0,z_sampled)
-        # Get prior.
-		z_prior_means,z_prior_sigs = self.prior(s0) 
+		s_T_mean, s_T_sig = self.dynamics_model(s_0,z_sampled)
+		# Get prior.
+		z_prior_means,z_prior_sigs = self.prior(s_0) 
 
 
-        z_post_dist = Normal.Normal(z_post_means, z_post_sigs)
-        s_T_dist = Normal.Normal(s_T_mean, s_T_sig )
+		z_post_dist = Normal.Normal(z_post_means, z_post_sigs)
+		s_T_dist = Normal.Normal(s_T_mean, s_T_sig )
 		z_prior_dist = Normal.Normal(z_prior_means, z_prior_sigs) 
 
 
@@ -88,8 +95,8 @@ class DynamicsModel(nn.Module):
 		return  loss, s_T_loss, kl_loss
 
 
-    
-    
+	
+	
 
 
 
@@ -97,109 +104,119 @@ class DynamicsModel(nn.Module):
 
 class TwoStageSkillModel(nn.Module):
 
-    def __init__(self,state_dim,a_dim,z_dim,h_dim,a_dist,stage,path,skill_beta=1.0,model_beta=1.0,fixed_sig=None,per_element_sigma=True,lr=5e-5,wd=0.0):
-        super().__init__()
-        assert stage in ['train_skills','train_dynamics']
-        self.stage = stage
-        self.path = path
-        self.model_beta = model_beta
-        self.skill_beta = skill_beta
-        self.skill_model = SkillPolicyModel(state_dim,a_dim,z_dim,h_dim,skill_beta,a_dist,fixed_sig=None)
-        self.dymamics_model = DynamicsModel(state_dim,z_dim,h_dim,model_beta,per_element_sigma)
+	def __init__(self,state_dim,a_dim,z_dim,h_dim,a_dist,stage,path,skill_beta=1.0,model_beta=1.0,fixed_sig=None,per_element_sigma=True,lr=5e-5,wd=0.0):
+		super().__init__()
+		assert stage in ['train_skills','train_dynamics']
+		self.stage = stage
+		self.path = path
+		self.model_beta = model_beta
+		self.skill_beta = skill_beta
+		self.skill_model = SkillPolicyModel(state_dim,a_dim,z_dim,h_dim,skill_beta,a_dist,fixed_sig=None)
+		self.dynamics_model = DynamicsModel(state_dim,a_dim,z_dim,h_dim,model_beta,per_element_sigma)
+		self.state_dim = state_dim
 
-        if stage = 'train_skills':
-            self.optimizer = torch.optim.Adam(self.skill_model.parameters(), lr=lr, weight_decay=wd)
-        elif stage == 'train_dynamics':
-            self.E_optimizer = torch.optim.Adam(self.dynamics_model.encoder.parameters(),   lr=lr, weight_decay=wd)
-            self.M_optimizer = torch.optim.Adam(self.dynamics_model.gen_model.parameters(), lr=lr, weight_decay=wd)
+		if stage == 'train_skills':
+			self.optimizer = torch.optim.Adam(self.skill_model.parameters(), lr=lr, weight_decay=wd)
+		elif stage == 'train_dynamics':
+			self.E_optimizer = torch.optim.Adam(self.dynamics_model.encoder.parameters(),   lr=lr, weight_decay=wd)
+			self.M_optimizer = torch.optim.Adam(self.dynamics_model.gen_model.parameters(), lr=lr, weight_decay=wd)
 
-        else:
-            assert False
-
-
-        
-
-        if stage == 'train_dynamics':
-            print('LOADING SKILL MODEL FROM FILE')
-            # load skill model from file
-            path = os.path.join(path,'log_best.pth')  # create skill path
-            # load skill model from skill path 
-            checkpoint = torch.load(path)
-            self.skill_model.load_state_dict(checkpoint['skill_model_state_dict'])
+		else:
+			assert False
 
 
-    def train(self,data):
+		
 
-        data = data.cuda()
-		states = data[:,:,:model.state_dim]
-		actions = data[:,:,model.state_dim:]
-
-        if stage == 'train_skills':
-            # get action losses
-            loss,a_loss,kl_loss = self.skill_model.get_losses(states,actions)
-
-            # perform training update
-            self.skill_model.zero_grad()
-		    loss.backward()
-		    optimizer.step()
-
-            return loss,a_loss,kl_loss
+		if stage == 'train_dynamics':
+			print('LOADING SKILL MODEL FROM FILE')
+			# load skill model from file
+			path = os.path.join(path,'log_best.pth')  # create skill path
+			# load skill model from skill path 
+			checkpoint = torch.load(path)
+			self.skill_model.load_state_dict(checkpoint['skill_model_state_dict'])
 
 
-        elif stage == 'train_dynamics':
-            ########### E STEP ###########
-            E_loss = self.get_E_loss(states,actions)
-            self.dynamics_model.zero_grad()
-            E_loss.backward()
-            self.E_optimizer.step()
+	def train(self,data):
 
-            ########### M STEP ###########
-            M_loss = self.get_M_loss(states,actions)
-            self.dynamics_model.zero_grad()
-            M_loss.backward()
-            M_optimizer.step()
+		data = data.cuda()
+		states = data[:,:,:self.state_dim]
+		actions = data[:,:,self.state_dim:]
 
-            return E_loss,M_loss
+		if self.stage == 'train_skills':
+			# get action losses
+			loss,a_loss,kl_loss = self.skill_model.get_losses(states,actions)
 
-        else:
-            assert False
-    
-    def validate(self,data):
-        with torch.no_grad():
-            data = data.cuda()
-            states = data[:,:,:model.state_dim]
-            actions = data[:,:,model.state_dim:]
+			# perform training update
+			self.skill_model.zero_grad()
+			loss.backward()
+			# print(self.skill_model.encoder.emb_layer[0].weight.grad)
+			# print(loss.grad)
+			self.optimizer.step()
+			# print('loss: ', loss)
+			return loss,a_loss,kl_loss
 
-            # if we're in the skill learning portion, get the "normal" skill learning losses
-            if self.stage == 'skill_learning':
-                
-                # get action losses
-                loss,a_loss,kl_loss = self.skill_model.get_losses(states,actions)
 
-                return loss,a_loss,kl_loss
+		elif self.stage == 'train_dynamics':
+			########### E STEP ###########
+			E_loss = self.get_E_loss(states,actions)
+			self.dynamics_model.zero_grad()
+			E_loss.backward()
+			self.E_optimizer.step()
 
-            else:
-                
-                loss,sT_loss,kl_loss = self.dynamics_model.get_losses(states,actions):
-                return loss, sT_loss,kl_loss
+			########### M STEP ###########
+			M_loss = self.get_M_loss(states,actions)
+			self.dynamics_model.zero_grad()
+			M_loss.backward()
+			self.M_optimizer.step()
 
-        
-    def get_E_loss(self,states,actions):
+			return E_loss,M_loss
 
-        assert self.encoder_type == 'state_action_sequence'
-		assert not self.state_dec_stop_grad
+		else:
+			assert False
+	
+	def validate(self,data):
+		with torch.no_grad():
+			data = data.cuda()
+			states = data[:,:,:self.state_dim]
+			actions = data[:,:,self.state_dim:]
 
-        assert self.model_beta == 1.0
+			# if we're in the skill learning portion, get the "normal" skill learning losses
+			if self.stage == 'train_skills':
+				
+				# get action losses
+				loss,a_loss,kl_loss = self.skill_model.get_losses(states,actions)
+				return loss,a_loss,kl_loss
+
+			elif self.stage == 'train_dynamics':
+				
+				loss,sT_loss,kl_loss = self.dynamics_model.get_losses(states,actions)
+				return loss, sT_loss,kl_loss
+			
+			else:
+				assert False
+
+		
+	def get_E_loss(self,states,actions):
+
+		# assert not self.state_dec_stop_grad
+		assert self.stage == 'train_dynamics'
+
+		assert self.model_beta == 1.0
 
 		batch_size,T,_ = states.shape
 		denom = T*batch_size
 		# get KL divergence between approximate and true posterior
 		z_post_means,z_post_sigs = self.dynamics_model.encoder(states,actions)
 
-		z_sampled = self.reparameterize(z_post_means,z_post_sigs)
+		z_sampled = reparameterize(z_post_means,z_post_sigs)
 
-		z_prior_means,z_prior_sigs = self.dynammics_model.prior(states[:,0:1,:]) 
-		a_means,a_sigs = self.skill_model.ll_policy(states,z_sampled)
+		z_prior_means,z_prior_sigs = self.dynamics_model.prior(states[:,0:1,:]) 
+		if self.skill_model.a_dist != 'autoregressive': 
+			a_means, a_sigs = self.skill_model.ll_policy(states,z_sampled)
+		else:
+			a_means, a_sigs = self.skill_model.ll_policy(states,actions,z_sampled)
+		a_dist = Normal.Normal(a_means, a_sigs)
+		# a_means,a_sigs = self.skill_model.ll_policy(states,z_sampled)
 
 		post_dist = Normal.Normal(z_post_means,z_post_sigs)
 		a_dist    = Normal.Normal(a_means,a_sigs)
@@ -212,21 +229,23 @@ class TwoStageSkillModel(nn.Module):
 
 		return -log_pi + -self.model_beta*log_prior + self.model_beta*log_post
 
-    # def get_M_loss(self):
-    #     # TODO
-    #     pass
+	# def get_M_loss(self):
+	#     # TODO
+	#     pass
 
-    def get_M_loss(self,states,actions):
+	def get_M_loss(self,states,actions):
+		assert self.stage == 'train_dynamics'
 
-        batch_size,T,_ = states.shape
+		batch_size,T,_ = states.shape
+		s_0 = states[:,0:1,:]
 		denom = T*batch_size
 		
-		z_post_means,z_post_sigs = self.dymamics_model.encoder(states,actions)
+		z_post_means,z_post_sigs = self.dynamics_model.encoder(states,actions)
 
-		z_sampled = self.reparameterize(z_post_means,z_post_sigs)
+		z_sampled = reparameterize(z_post_means,z_post_sigs)
 
-		z_prior_means,z_prior_sigs = self.dynamics_model.prior(states[:,0:1,:]) 
-		sT_mean, sT_sig = self.dynamics_model.dynamics_model(states,z_sampled)
+		z_prior_means,z_prior_sigs = self.dynamics_model.prior(s_0) 
+		sT_mean, sT_sig = self.dynamics_model.dynamics_model(s_0,z_sampled)
 
 		sT_dist  = Normal.Normal(sT_mean,sT_sig)
 		prior_dist = Normal.Normal(z_prior_means,z_prior_sigs)
@@ -235,30 +254,29 @@ class TwoStageSkillModel(nn.Module):
 		sT_loss = -torch.sum(sT_dist.log_prob(sT)) / denom
 		prior_loss = -torch.sum(prior_dist.log_prob(z_sampled)) / denom
 
-		return sT_loss + self.beta*prior_loss
+		return sT_loss + self.model_beta*prior_loss
 
 
-    def save_checkpoint(self,filename):
-        # TODO
-        path = os.path.join(self.path,filename)
-        if self.stage == 'train_skills':
-            torch.save({
-                'skill_model_state_dict': self.skill_model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                }, self.path)
-        else:
-            torch.save({
-                'skill_model_state_dict': self.skill_model.state_dict(),
-                'dynamics_model_state_dict':self.
-                'optimizer_state_dict': self.optimizer.state_dict(),
-                'E_optimizer_state_dict': self.E_optimizer.state_dict(),
-                'M_optimizer_state_dict': self.M_optimizer.state_dict()
-                }, self.path)
+	def save_checkpoint(self,filename):
+		# TODO
+		path = os.path.join(self.path,filename)
+		if self.stage == 'train_skills':
+			torch.save({
+				'skill_model_state_dict': self.skill_model.state_dict(),
+				'optimizer_state_dict': self.optimizer.state_dict(),
+				}, path)
+		else:
+			torch.save({
+				'skill_model_state_dict': self.skill_model.state_dict(),
+				'dynamics_model_state_dict':self.dynamics_model.state_dict(),
+				'E_optimizer_state_dict': self.E_optimizer.state_dict(),
+				'M_optimizer_state_dict': self.M_optimizer.state_dict()
+				}, path)
 
 
-    def load_from_checkpoint(self,filename):
-        # TODO
-        pass
+	def load_from_checkpoint(self,filename):
+		# TODO
+		pass
 
 
 
