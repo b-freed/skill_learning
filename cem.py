e3f8a5b6e5e4189073e2a3a3fa78bc638c2eb928
b-freed <bfreed@andrew.cmu.edu>
2022-06-07 13:45:10 -0400

    remove print statements from cem.py

--- cem.py
+++ cem.py
@@ -54,8 +54,46 @@ def cem_iter(x,cost_fn,frac_keep,l2_pen):
     x_std  = torch.std( x_topk,dim=0)
     # print('x_std: ', x_std)
     # ipdb.set_trace()
-    print('torch.mean(costs): ', torch.mean(costs))
+    # print('torch.mean(costs): ', torch.mean(costs))
     return x_mean,x_std,cost_topk
+
+
+# def cem_iter_with_prior(x,cost_fn,frac_keep,prior_weight):
+#     '''
+#     INPUTS:
+#         x: N x _ tensor of initial solution candidates
+#         cost_fn: function that returns cost scores in the form of an N-dim tensor
+#     OUTPUTS:
+#         x_mean: _-dimensional tensor of mean of updated solution candidate population
+#         x_std:  _-dimensional tensor of stand dev of updated solution candidate population
+#         cost_topk:  scalar mean cost of updated solution candidates
+#     '''
+#     N = x.shape[0]
+#     k = int(N*frac_keep) # k is for keep y'all
+    
+#     # evaluate solution candidates, get sorted inds
+#     costs = cost_fn(x)
+#     # print('costs: ', costs)
+#     # l2_cost = l2_pen*torch.mean(torch.mean(x**2,dim=-1),dim=-1) 
+#     # costs += l2_cost
+#     inds = torch.argsort(costs)
+
+#     weights = gaussian_likelihood_ratio(# get likelihood ratios
+
+    
+#     # figure out which inds to keep
+#     inds_keep = inds[:k]
+#     # get best k solution candidates & their average cost
+#     x_topk = x[inds_keep,...]
+#     cost_topk = torch.mean(costs[inds_keep])
+#     # take mean and stand dev of new solution population
+#     x_mean = torch.mean(x_topk,dim=0)
+#     # print('x_mean: ', x_mean)
+#     x_std  = torch.std( x_topk,dim=0)
+#     # print('x_std: ', x_std)
+#     # ipdb.set_trace()
+#     print('torch.mean(costs): ', torch.mean(costs))
+#     return x_mean,x_std,cost_topk
 
 def cem(x_mean,x_std,cost_fn,pop_size,frac_keep,n_iters,l2_pen):
 
