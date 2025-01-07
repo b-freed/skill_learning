a5bc3dedeb5e500884177b3f8d0d554cc17d0401
b-freed <bfreed@andrew.cmu.edu>
2025-01-07 10:29:22 -0500

    cleanup

--- planning_videos.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from utils import make_video_from_dir
-
-
-make_video_from_dir('planning_imgs/run6','planning_antmaze_large_dense',trim_inds = (65,420,84,577))
--- random_actions.py
+++
@@ -1,5 +0,0 @@
-
-
-
-env = 'kitchen-partial-v0'
-env = gym.make(env)
